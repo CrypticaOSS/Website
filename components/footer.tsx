86d9dee0b78056825aa7394bca5d2840d4389a87
Connor200024 <connor@connorsdevelopment.xyz>
2026-08-28 23:07:09 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
   return (
     <footer className="border-t py-6 mt-10 text-center">
       <p className="text-sm text-muted-foreground">
-        © {new Date().getFullYear()} - 2025 Copyright - Cryptica. | Orginal Owner:{" "}
+        © 2025 - 2026 Copyright - Cryptica. | Orginal Owner:{" "}
         <a
           href="https://github.com/CodeMeAPixel"
           target="_blank"
