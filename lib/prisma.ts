59f413d8bb155c4068edabcf2ef9715cb48e9e25
Harley <harley@harley200317.xyz>
2026-09-04 11:46:50 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import "server-only"
 
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@/src/generated/prisma/client"
 
 const connectionString =
   process.env.DATABASE_URL
